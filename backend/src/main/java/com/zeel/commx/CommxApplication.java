ce94cfc75501776c4a64ae5acc9d6e2fcd00fcec
zeelsheth006 <zeelsheth1@gmail.com>
2026-02-28 11:17:58 +0530

    Your meaningful commit message here

---
+++ backend/src/main/java/com/zeel/commx/CommxApplication.java
@@ -0,0 +1,13 @@
+package com.zeel.commx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommxApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/zeel/commx/CommxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeel.commx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
